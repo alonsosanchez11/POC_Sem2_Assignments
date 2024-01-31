b43bc2219b41a3bc4e716a8990a87190361067d9
Alonso Sanchez <sanchalo000@warren.k12.in.us>
2024-01-31 09:34:50 -0500

    za

--- 03_Generators and Exceptions/03_Exception Hierarchy/assignment.py
+++ 03_Generators and Exceptions/03_Exception Hierarchy/assignment.py
@@ -1,20 +1,8 @@
-number1 = 0
-number2 = 0
-try:
-    number1 = int(input("Enter a number"))
-    # YOUDO.  use input function and int to set number2
+value1 = int(input('Enter integer 1: '))
+value2 = int(input('Enter integer 2: '))
+print('The inverse of', value1, 'is', 1/value1)
+print('The inverse of', value2, 'is', 1/value2)
 except ValueError:
-    # print message stating that an integer wasn't given
-    pass #YOUDO remove pass when done
-
-
-try:
-    #YOUDO divide number1 / number2 and set to answer
-    #YOUDO  print the result of the division (aka answer with some helper text)
-    pass  # YOUDO remove pass when done
-
+print('You did not provide a number, so I will not calculate the inverse')
 except ZeroDivisionError:
-    #YOUDO:  print message stating that division by zero is not possible.  
-    pass  # YOUDO remove pass when done
-
-    
+print('You provided 0 and division by 0 is not possible, sorry')
